09e064429a2a0a40ba36fb9ab08c5d8c0691317d
Web <web@it.step>
2022-10-25 18:26:06 +0300

    html+css v_2.0

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-const wordsArray = [
-    "дом",
-    "конфета",
-    "шар",
-    "праздник",
-    "слезы",
-    "синхофазотрончик",
-    "воздух",
-    "виселица",
-    "игра",
+import { randomWords, showImageStart, addKey, counterImg } from "./utils.js";
+import { createLetters } from "./createElements.js";
+import { chekValue } from "./chekValue.js";
+
+const arrWords = [
+  "truck",
+  "pencil",
+  "rain",
+  "banana",
+  "potato",
+  "car",
+  "mountain",
+  "bike",
+  "house",
+  "elephant",
 ];
+
+export const arrImage = [
+  "./img/1.png",
+  "./img/2.png",
+  "./img/3.png",
+  "./img/4.png",
+  "./img/pngtree-game-over.jpg",
+];
+
+export const input = document.getElementById("input");
+export const word = randomWords(arrWords);
+export const wordContainer = document.getElementById("containerWordLine");
+export const arrNodes = wordContainer.childNodes;
+export const img = document.createElement("img");
+export const resultCounter = counterImg();
+export const button = document.getElementById("button");
+const buttonStart = document.getElementById("buttonStart");
+
+console.log(word);
+
+
+export function startGame() {
+  let resultCounter = counterImg();
+  wordContainer.textContent = "";
+  createLetters(word);
+  showImageStart(arrImage);
+
+  button.addEventListener("click", chekValue);
+  input.addEventListener("keydown", addKey);
+  buttonStart.addEventListener("click", startGame);
+}
+startGame();
